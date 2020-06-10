30dddc5265d146265af4d0ede2eff41369ed0bb0
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-06-10 21:23:43 +0200

    refactor: add poetry and config
    Add poetry and update build

--- easyTemplate/__init__.py
+++ easyTemplate/__init__.py
@@ -1,4 +1,15 @@
 __author__ = "github.com/wardsimon"
-__version__ = "0.0.1"
 
-# import easyTemplate.Engines
+
+import sys
+
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+else:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0.dev"
